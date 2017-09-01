7d44616516b53c2b45b188fb18a0f78913ce05a5
Jeff Schomay <jschomay@gmail.com>
2017-08-31 18:09:41 -0600

    fixed exercises

---
+++ lib/planet.ex
@@ -0,0 +1,45 @@
+defmodule Planet do
+
+  import Calcs
+  import Converter
+  import Physics.Laws
+
+  defstruct [name: nil, type: :rocky, mass: 0, radius: 0, ev: 0]
+
+  defp planets do
+    [
+      {:mercury, %Planet{name: "Mercury", type: :rocky, mass: 3.3e23, radius: 2.439e6 }},
+      {:venus, %Planet{name: "Venus", type: :rocky, mass: 4.86e24, radius: 6.05e6 }},
+      {:earth, %Planet{name: "Earth", type: :rocky, mass: 5.972e24, radius: 6.37e6 }},
+      {:mars, %Planet{name: "Mars", type: :rocky, mass: 6.41e23, radius: 3.37e6 }},
+      {:jupiter, %Planet{name: "Jupiter", type: :gaseous, mass: 1.89e27, radius: 7.14e7 }},
+      {:saturn, %Planet{name: "Saturn", type: :gaseous, mass: 5.68e26, radius: 6.02e7 }},
+      {:uranus, %Planet{name: "Uranus", type: :gaseous, mass: 8.68e25, radius: 2.55e7 }},
+      {:neptune, %Planet{name: "Neptune", type: :gaseous, mass: 1.02e26, radius: 2.47e7 }},
+    ]
+  end
+
+
+  def select do
+    for {key, planet} <- planets,
+      ev <- [escape_velocity(planet)],
+      do: {key, %{planet | ev: ev}}
+  end
+
+  def escape_velocity, do: escape_velocity(select[:earth])
+
+  def escape_velocity(planet) when is_map(planet) do
+    planet
+      |> calculate_escape
+      |> to_km
+      |> to_nearest_tenth
+  end
+
+  def escape_velocity(:earth), do: escape_velocity(select[:earth])
+  def escape_velocity(:moon), do: escape_velocity(select[:moon])
+
+  defp calculate_escape(%{mass: mass, radius: radius}) do
+    2 * newtons_gravitational_constant * mass / radius
+      |> square_root
+  end
+end
--- test/planet_test.exs
+++ test/planet_test.exs
@@ -3,14 +3,14 @@ defmodule PlanetTest do
 
   setup do
     planets = [
-       %{name: "Mercury", type: :rocky, ev: 4.3},
-       %{name: "Venus", type: :rocky, ev: 10.4},
-       %{name: "Earth", type: :rocky, ev: 11.2},
-       %{name: "Mars", type: :rocky, ev: 5.0},
-       %{name: "Jupiter", type: :gaseous, ev: 59.5},
-       %{name: "Saturn", type: :gaseous, ev: 35.5},
-       %{name: "Uranus", type: :gaseous, ev: 21.3},
-       %{name: "Neptune", type: :gaseous, ev: 23.5}
+      %{name: "Mercury", type: :rocky, ev: 4.3},
+      %{name: "Venus", type: :rocky, ev: 10.4},
+      %{name: "Earth", type: :rocky, ev: 11.2},
+      %{name: "Mars", type: :rocky, ev: 5.0},
+      %{name: "Jupiter", type: :gaseous, ev: 59.5},
+      %{name: "Saturn", type: :gaseous, ev: 35.5},
+      %{name: "Uranus", type: :gaseous, ev: 21.3},
+      %{name: "Neptune", type: :gaseous, ev: 23.5}
     ]
     {:ok, planets: planets}
   end
@@ -38,25 +38,22 @@ defmodule PlanetTest do
 
   test "Enum.map", %{planets: planets} do
     p = Enum.map(planets, &(&1.name))
-    #this should be a list of names... assert for me?
+    assert p == ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
   end
 
   test "a comprehension", %{planets: planets} do
     planet_names = for planet <- planets, do: planet.name  
-    IO.inspect planet_names
-    #this should be a list of names... assert for me?
+    assert planet_names == ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
   end
 
   test "a comprehension with a filter", %{planets: planets} do
     planet_names = for planet <- planets, planet.type == :rocky, do: planet.name  
-    IO.inspect planet_names
-    #this should be a list of names... assert for me?
+    assert planet_names == ["Mercury", "Venus", "Earth", "Mars"]
   end
 
   test "a comprehension with a filter, matched", %{planets: planets} do
     planet_names = for %{name: name, type: type} <- planets, type == :rocky, do: name  
-    IO.inspect planet_names
-    #this should be a list of names... assert for me?
+    assert planet_names == ["Mercury", "Venus", "Earth", "Mars"]
   end
 
 end
